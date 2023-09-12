4026668aadbb214e85321f27f5ab4ecffd17badf
Michlit <98559228+Michlit7@users.noreply.github.com>
2023-09-12 21:15:47 +0200

    Add font from design

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export function Button({ text }: Props) {
   return (
-    <button className="border-6 border-blue-3 bg-blue-2 px-7 py-3.5 text-xl font-bold leading-none text-white">
+    <button className="border-6 border-blue-3 bg-blue-2 px-7 py-3.5 text-xl leading-none text-white">
       {text}
     </button>
   );
