e29a4a0eb7db6af5ab5c6e763d9b2c1c5cf7c4ac
Kacper <33208083+demondobf@users.noreply.github.com>
2023-09-24 03:01:14 +0200

    Re-add page interactions

--- src/app/[seed]/page.tsx
+++ src/app/[seed]/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/button";
 import { SpotsList } from "@/components/spots-list";
@@ -12,15 +15,29 @@ interface Props {
 export default function Page({ params }: Props) {
   const seed = params.seed;
 
+  const [currentItemIndex, setCurrentItemIndex] = useState(0);
+
+  function incrementItemIndex() {
+    setCurrentItemIndex((a) => a + 1);
+  }
+
+  function decrementItemIndex() {
+    setCurrentItemIndex((a) => a - 1);
+  }
+
   return (
     <main>
       <div className="bg-gradient-radial from-blue-2 to-blue-1 p-4 text-white">
         <Logo />
-        <SpotsList items={getRandomLocations(seed)} currentItemIndex={0} />
+
+        <SpotsList
+          items={getRandomLocations(seed)}
+          currentItemIndex={currentItemIndex}
+        />
 
         <div className="mt-1 grid grid-cols-2 gap-1">
-          <Button text="Previous" />
-          <Button text="Next" />
+          <Button text="Previous" onClick={decrementItemIndex} />
+          <Button text="Next" onClick={incrementItemIndex} />
         </div>
 
         <div className="mb-5 mt-16 grid grid-rows-1">
